621ac9185304478816c2715bdb5159b37ffb9aae
R N pathak <rajiv.nayan2021@vitbhopal.ac.in>
2024-02-18 09:40:32 +0530

    Add files via upload

---
+++ 01_pattern/pattern_03.cpp
@@ -0,0 +1,28 @@
+/*
+    pattern for n = 4 ;
+
+                16  15  14  13
+                12  11  10  9
+                8   7   6   5   
+                4   3   2   1
+*/
+
+// code :-
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,value;
+    cout<<"Enter the number n: "<<endl;
+    cin>>n;
+    value = n*n;
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=n;j++){
+            cout<<value<<" ";
+            value--;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
